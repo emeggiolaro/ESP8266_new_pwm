c339d4f32d311c78e094f24c44179cf95949b92a
emeggiolaro <info@ingem.it>
2019-10-06 21:21:07 +0200

    Divisione .h e .cpp

--- pwm.cpp
+++ pwm.cpp
@@ -16,46 +16,11 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
-/* Set the following three defines to your needs */
-
-#ifndef SDK_PWM_PERIOD_COMPAT_MODE
-  #define SDK_PWM_PERIOD_COMPAT_MODE 0
-#endif
-#ifndef PWM_MAX_CHANNELS
-  #define PWM_MAX_CHANNELS 8
-#endif
-#define PWM_DEBUG 0
-#define PWM_USE_NMI 0
-
-/* no user servicable parts beyond this point */
-
-#define PWM_MAX_TICKS 0x7fffff
-#if SDK_PWM_PERIOD_COMPAT_MODE
-#define PWM_PERIOD_TO_TICKS(x) (x * 0.2)
-#define PWM_DUTY_TO_TICKS(x) (x * 5)
-#define PWM_MAX_DUTY (PWM_MAX_TICKS * 0.2)
-#define PWM_MAX_PERIOD (PWM_MAX_TICKS * 5)
-#else
-#define PWM_PERIOD_TO_TICKS(x) (x)
-#define PWM_DUTY_TO_TICKS(x) (x)
-#define PWM_MAX_DUTY PWM_MAX_TICKS
-#define PWM_MAX_PERIOD PWM_MAX_TICKS
-#endif
-
 #include <c_types.h>
-#include <pwm.h>
+#include "pwm.h"
 #include <eagle_soc.h>
 #include <ets_sys.h>
 
-// from SDK hw_timer.c
-#define TIMER1_DIVIDE_BY_16             0x0004
-#define TIMER1_ENABLE_TIMER             0x0080
-
-struct pwm_phase {
-	uint32_t ticks;    ///< delay until next phase, in 200ns units
-	uint16_t on_mask;  ///< GPIO mask to switch on
-	uint16_t off_mask; ///< GPIO mask to switch off
-};
 
 /* Three sets of PWM phases, the active one, the one used
  * starting with the next cycle, and the one updated
@@ -64,12 +29,7 @@ struct pwm_phase {
  * pwm_next_set from the interrupt routine during the first
  * pwm phase
  */
-typedef struct pwm_phase (pwm_phase_array)[PWM_MAX_CHANNELS + 2];
 static pwm_phase_array pwm_phases[3];
-static struct {
-	struct pwm_phase* next_set;
-	struct pwm_phase* current_set;
-	uint8_t current_phase;
 } pwm_state;
 
 static uint32_t pwm_period;
@@ -78,36 +38,9 @@ static uint32_t pwm_duty[PWM_MAX_CHANNELS];
 static uint16_t gpio_mask[PWM_MAX_CHANNELS];
 static uint8_t pwm_channels;
 
-// 3-tuples of MUX_REGISTER, MUX_VALUE and GPIO number
-typedef uint32_t (pin_info_type)[3];
-
-struct gpio_regs {
-	uint32_t out;         /* 0x60000300 */
-	uint32_t out_w1ts;    /* 0x60000304 */
-	uint32_t out_w1tc;    /* 0x60000308 */
-	uint32_t enable;      /* 0x6000030C */
-	uint32_t enable_w1ts; /* 0x60000310 */
-	uint32_t enable_w1tc; /* 0x60000314 */
-	uint32_t in;          /* 0x60000318 */
-	uint32_t status;      /* 0x6000031C */
-	uint32_t status_w1ts; /* 0x60000320 */
-	uint32_t status_w1tc; /* 0x60000324 */
-};
 static struct gpio_regs* gpio = (struct gpio_regs*)(0x60000300);
-
-struct timer_regs {
-	uint32_t frc1_load;   /* 0x60000600 */
-	uint32_t frc1_count;  /* 0x60000604 */
-	uint32_t frc1_ctrl;   /* 0x60000608 */
-	uint32_t frc1_int;    /* 0x6000060C */
-	uint8_t  pad[16];
-	uint32_t frc2_load;   /* 0x60000620 */
-	uint32_t frc2_count;  /* 0x60000624 */
-	uint32_t frc2_ctrl;   /* 0x60000628 */
-	uint32_t frc2_int;    /* 0x6000062C */
-	uint32_t frc2_alarm;  /* 0x60000630 */
-};
 static struct timer_regs* timer = (struct timer_regs*)(0x60000600);
+
 
 static void ICACHE_RAM_ATTR
 pwm_intr_handler(void)
